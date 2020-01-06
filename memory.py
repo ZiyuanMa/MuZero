481abcce4daf04daeae8f1ac91601cd609397075
ziyuanma <ziyuanm@sfu.ca>
2020-01-05 18:04:38 -0800

    fix init board

--- memory.py
+++ memory.py
@@ -12,27 +12,25 @@ class Memory:
         board[4][3] = -1
         self.dict_list[0][board.tobytes()] = [0, 0, 1]
         self.init_layer = 0
-        self.aval_num = 1
         self.init_list = [(board.tobytes(), 1)]
         #print(self.dict_list)
 
         #print(len(self.dict_list[0]))
 
     def get_init_board(self):
+        if len(self.init_list) == 0:
+            raise RuntimeError('no board in init list')
 
         bytes_board, next = random.choice(self.init_list)
-        while self.dict_list[self.init_layer][bytes_board][1] >= 100:
-            bytes_board, next = random.choice(self.init_list)
 
         if self.dict_list[self.init_layer][bytes_board][1] == 99:
-            self.aval_num -= 1
-            if self.aval_num == 0:
+            self.init_list.remove((bytes_board, next))
+            if len(self.init_list) == 0:
                 self.init_layer += 1
                 self.init_list = [ (key, value[2]) for key, value in self.dict_list[self.init_layer].items() 
                                         if self.dict_list[self.init_layer][key][1] < 100 ]
-                self.aval_num = len(self.init_list)
 
-                if self.aval_num == 0:
+                if len(self.init_list) == 0:
                     raise RuntimeError('no avaliable board')
 
         board = np.frombuffer(bytes_board, dtype='int8').reshape(8,8)
@@ -52,8 +50,6 @@ class Memory:
             if self.dict_list[num][bytes_board][2] != next:
                 raise RuntimeError('wrong next')
             self.dict_list[num][bytes_board][1] += 1
-
-
 
 
     def __len__(self):
